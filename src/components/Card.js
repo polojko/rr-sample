789434158f88296062074f87ad2ea6e4b3bb4920
polojko <polojko@gmail.com>
2020-10-19 19:49:39 -0700

    Adding persistent card checkbox

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-const Card = ({card}) => {
+const Card = ({ card }) => {
+
+    const getLocalStorageItem = (id) => {
+        let parseToBool = JSON.parse(localStorage.getItem(id))
+        if (parseToBool) {
+            return parseToBool
+        }
+    }
+
     return (
         <div key={card.id} className='card'>
             <img src={card.imageUrl} alt={card.name} />
@@ -9,6 +17,15 @@ const Card = ({card}) => {
             <div className='other-fields'>
                 {card.set.name && <span><b>Set:</b> {card.set.name} </span>}
                 {card.type && <span><b>Type:</b> {card.type}</span>}
+            </div>
+            <div className='select-card'>
+                <label htmlFor={card.id}>Select card:</label>
+                <input
+                    id={card.id}
+                    type='checkbox'
+                    defaultChecked={getLocalStorageItem(card.id)}
+                    onChange={(e) => localStorage.setItem(card.id, e.target.checked)}
+                />
             </div>
         </div>
     )
